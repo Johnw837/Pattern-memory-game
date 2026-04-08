437d3922296c9f42453cbe8080c4e6d6f4f6cc2b
Johnw837 <johnwatt837@gmail.com>
2026-04-08 18:14:44 +0300

    Add game menu functions for starting game and alerts

---
+++ menu.js
@@ -0,0 +1,21 @@
+function startGame(){
+
+document.getElementById("menu").style.display="none";
+
+document.getElementById("gameUI").style.display="block";
+
+createBoard();
+
+}
+
+function openLeaderboard(){
+
+alert("Leaderboard coming soon");
+
+}
+
+function openRewards(){
+
+alert("Rewards coming soon");
+
+}
